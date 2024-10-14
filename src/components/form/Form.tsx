df6f0637889634b3cdbb60c715a0d37b3e9a9495
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-10-14 16:43:11 -0400

    creado formulario para seleccionar pais y ciudad

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,27 @@
+import { countries } from "../../data/countries";
+import styles from "./Form.module.css";
+
+export default function Form() {
+  return (
+    <form className={styles.form}>
+      <div className={styles.field}>
+        <label htmlFor="city">Ciudad:</label>
+        <input id="city" type="text" name="city" placeholder="Ciudad" />
+      </div>
+
+      <div className={styles.field}>
+        <label htmlFor="city">País:</label>
+        <select>
+          <option value="">-- Seleccione un País ---</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
+    </form>
+  );
+}
