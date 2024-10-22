6a0be0ec78471d499ced923e78edba98935a89be
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-10-22 16:25:55 -0400

    Agregado useState para search y funcion handleChange

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,17 +1,31 @@
+import { ChangeEvent, useState } from "react";
 import { countries } from "../../data/countries";
 import styles from "./Form.module.css";
+import { SearchType } from "../../types";
 
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form className={styles.form}>
       <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
-        <input id="city" type="text" name="city" placeholder="Ciudad" />
+        <input id="city" type="text" name="city" placeholder="Ciudad" value={search.city} onChange={handleChange} />
       </div>
 
       <div className={styles.field}>
         <label htmlFor="city">País:</label>
-        <select>
+        <select id="country" name="country" value={search.country} onChange={handleChange}>
           <option value="">-- Seleccione un País ---</option>
           {countries.map((country) => (
             <option key={country.code} value={country.code}>
