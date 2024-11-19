fca54fe4675bc4bb6d70d2732841d9f5fa1b0ec7
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-11-19 09:07:30 -0400

    Forma2 de tipar una respuesta usando TypeGuard

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
 import { SearchType, Wheather } from "../types";
 
+// TYPE GUARD O ASSERTION
+function isWeatherResponse(weather: unknown): weather is Wheather {
+  return (
+    Boolean(weather) &&
+    typeof weather === "object" &&
+    typeof (weather as Wheather).name === "string" &&
+    typeof (weather as Wheather).main.temp === "number" &&
+    typeof (weather as Wheather).main.temp_max === "number" &&
+    typeof (weather as Wheather).main.temp_min === "number"
+  );
+}
+
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
     const appId = import.meta.env.VITE_API_KEY;
@@ -12,13 +24,23 @@ export default function useWeather() {
       const lon = data[0].lon;
 
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
-      const { data: weatherData } = await axios<Wheather>(weatherUrl);
 
-      console.log(weatherData);
-      console.log(weatherData.name);
-      console.log(weatherData.main.temp);
-      console.log(weatherData.main.temp_max);
-      console.log(weatherData.main.temp_min);
+      //Castear el type para el response
+      // const { data: weatherData } = await axios<Wheather>(weatherUrl);
+      // console.log(weatherData);
+      // console.log(weatherData.name);
+      // console.log(weatherData.main.temp);
+      // console.log(weatherData.main.temp_max);
+      // console.log(weatherData.main.temp_min);
+
+      // Type Guards
+      const { data: weatherResult } = await axios(weatherUrl);
+      const result = isWeatherResponse(weatherResult);
+      if (result) {
+        console.log(weatherResult.name);
+      } else {
+        console.log("Respuesta mal formada");
+      }
     } catch (error) {
       console.log(error);
     }
