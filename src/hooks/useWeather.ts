f3644441cc5ad233f8f66f4c973211b511782fed
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-11-19 08:41:42 -0400

    usando variables de entorno en vite con import.meta.env

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -3,7 +3,7 @@ import { SearchType } from "../types";
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
-    const appId = "1eda1a25e5db71b3f7b7a287873846b0";
+    const appId = import.meta.env.VITE_API_KEY;
 
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
